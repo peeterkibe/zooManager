3eac7a93f2d5e9ded20ed7f57344e5b8526dad5f
armin400 <81504154+armin400@users.noreply.github.com>
2021-04-11 13:09:04 +0300

    Update MainMenu.java

--- src/main/java/menus/MainMenu.java
+++ src/main/java/menus/MainMenu.java
@@ -1,4 +1,15 @@
-package menus;
+package menu;
+
+import menus.SubMenu;
+import util.DBUtil;
+
+import java.util.Scanner;
 
 public class MainMenu {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SubMenu subMenu = new SubMenu();
+        subMenu.login(input);
+    }
 }

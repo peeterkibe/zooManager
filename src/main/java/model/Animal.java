f07b059492168e6f7c37ef9b4868ee90c458027f
peeterkibe <peeter.kibe@gmail.com>
2021-04-11 10:56:28 +0300

    animals and animaltype

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -1,4 +1,39 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idSpecies;
+    private String speciesName;
+
+    public int getIdSpecies() {
+        return idSpecies;
+    }
+
+    public void setIdSpecies(int idSpecies) {
+        this.idSpecies = idSpecies;
+    }
+
+    public String getSpeciesName() {
+        return speciesName;
+    }
+
+    public void setSpeciesName(String speciesName) {
+        this.speciesName = speciesName;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "idSpecies=" + idSpecies +
+                ", speciesName='" + speciesName + '\'' +
+                '}';
+    }
 }
--- src/main/java/model/AnimalType.java
+++ src/main/java/model/AnimalType.java
@@ -1,4 +1,58 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class AnimalType {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private int idAnimalType;
+     private String animalType;
+     private String feed;
+     private int speciesOfAnimals;
+
+    public int getIdAnimalType() {
+        return idAnimalType;
+    }
+
+    public void setIdAnimalType(int idAnimalType) {
+        this.idAnimalType = idAnimalType;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public String getFeed() {
+        return feed;
+    }
+
+    public void setFeed(String feed) {
+        this.feed = feed;
+    }
+
+    public int getSpeciesOfAnimals() {
+        return speciesOfAnimals;
+    }
+
+    public void setSpeciesOfAnimals(int speciesOfAnimals) {
+        this.speciesOfAnimals = speciesOfAnimals;
+    }
+
+    @Override
+    public String toString() {
+        return "AnimalType{" +
+                "idAnimalType=" + idAnimalType +
+                ", animalType='" + animalType + '\'' +
+                ", feed='" + feed + '\'' +
+                ", speciesOfAnimals=" + speciesOfAnimals +
+                '}';
+    }
 }

83e019c445c3a64770956d6d0de4d18aeb206712
peeterkibe <peeter.kibe@gmail.com>
2021-04-11 11:14:50 +0300

    Jobs, Schedule and Veterinarian

--- src/main/java/model/Veterinarian.java
+++ src/main/java/model/Veterinarian.java
@@ -1,4 +1,69 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Veterinarian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idVeterinarian;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+
+    public int getIdVeterinarian() {
+        return idVeterinarian;
+    }
+
+    public void setIdVeterinarian(int idVeterinarian) {
+        this.idVeterinarian = idVeterinarian;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Veterinarian{" +
+                "idVeterinarian=" + idVeterinarian +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }

235faccc4673b5aaf4b74f2898ace19f6390923c
peeterkibe <peeter.kibe@gmail.com>
2021-04-11 12:04:30 +0300

    handler, job, schedule and vet repo

--- src/main/java/persistence/VeterinarianRepository.java
+++ src/main/java/persistence/VeterinarianRepository.java
@@ -1,4 +1,44 @@
 package persistence;
 
+import model.Veterinarian;
+import util.DBUtil;
+
+import javax.persistence.EntityManager;
+
 public class VeterinarianRepository {
+    private EntityManager em;
+
+    public VeterinarianRepository() {
+        this.em = DBUtil.getEntityManager();
+    }
+
+    public void addVeterinarian(Veterinarian veterinarian){
+        try {
+            this.em.getTransaction().begin();
+            this.em.persist(veterinarian);
+            this.em.getTransaction().commit();
+        } catch (Exception e) {
+            this.em.getTransaction().rollback();
+        }
+    }
+
+    public void updateVeterinarian (Veterinarian veterinarian){
+        try {
+            this.em.getTransaction().begin();
+            this.em.merge(veterinarian);
+            this.em.getTransaction().commit();
+        } catch (Exception e) {
+            this.em.getTransaction().rollback();
+        }
+    }
+
+    public void deleteVeterinarian (Veterinarian veterinarian){
+        try {
+            this.em.getTransaction().begin();
+            this.em.remove(em.merge(veterinarian));
+            this.em.getTransaction().commit();
+        } catch (Exception e) {
+            this.em.getTransaction().rollback();
+        }
+    }
 }

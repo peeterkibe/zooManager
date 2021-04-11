682b7946e1bd035e0076087a2a524f92397d8cf3
armin400 <81504154+armin400@users.noreply.github.com>
2021-04-11 11:12:34 +0300

    Cage and Handler

--- src/main/java/model/Cage.java
+++ src/main/java/model/Cage.java
@@ -1,4 +1,27 @@
 package model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Cage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCage;
+    private String enclosureName;
+
+    public int getIdCage() { return idCage; }
+
+    public void setIdCage(int idCage) { this.idCage = idCage;}
+
+    public String getEnclosureName() { return enclosureName; }
+
+    public void setEnclosureName(String enclosureName) { this.enclosureName = enclosureName; }
+
+
 }
--- src/main/java/model/Handler.java
+++ src/main/java/model/Handler.java
@@ -1,4 +1,38 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Handler {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int idHandler;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    public int getIdHandler() { return idHandler; }
+
+    public void setIdHandler(int idHandler) { this.idHandler = idHandler; }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+    
 }
